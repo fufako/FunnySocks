8cd29f8b3c6c1d737620ad6b3658ceb5856c2a5b
fufako <patryko.mikiciuk@gmail.com>
2022-09-14 15:22:19 +0200

    Setup components, setup images

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <div className="header">
+      <h1>Funny Socks Shop</h1>
+    </div>
+  )
+}
+export default Home
---
+++ src/components/Products.js
@@ -0,0 +1,8 @@
+function Products() {
+  return (
+    <div className="header">
+      <h1>Funny Socks Shop</h1>
+    </div>
+  )
+}
+export default Products
