0da3d54ea2f3493b1892aafe5e5992fd17fdf5db
fufako <patryko.mikiciuk@gmail.com>
2022-09-15 10:34:12 +0200

    Display message if the cart is empty

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,17 +3,16 @@ import CartItem from "./CartItem"
 import EmptyCart from "./EmptyCart"
 function Cart(props) {
   const { items } = props
-  const emptyInfo = document.querySelector(".empty")
   return (
     <div className="main-cart-container">
       <div className="cart">
         <h3 className="cart-title">YOUR SHOPPING CART</h3>
-        {items ? (
+        {items.length > 1 ? (
           items.map((item) => <CartItem item={item} key={item.key} />)
         ) : (
-          <EmptyCart></EmptyCart>
+          <EmptyCart />
         )}
-        <div className="empty"></div>
+
         <button className="payment">Proceed to payment</button>
         <Link to="/products" className="back-to-shop">
           Back to shop
