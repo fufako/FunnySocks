1397df4e56247d6df208fe79e3753d06a4010207
fufako <patryko.mikiciuk@gmail.com>
2022-09-15 11:11:53 +0200

    Add styles for shopping cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,8 +2,19 @@ function CartItem(props) {
   const { item } = props
 
   return (
-    <div>
-      <h5>{item.name}</h5>
+    <div className="items-container">
+      <div className="item-container">
+        <img src={item.src} alt={item.name} className="item-cart-img" />
+        <div className="item-info">
+          <h5 className="item-cart-name">{item.name}</h5>
+          <p className="cart-price">{item.price}</p>
+          <div className="add-remove">
+            <button>+</button>
+            <input type="number"></input>
+            <button>-</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
